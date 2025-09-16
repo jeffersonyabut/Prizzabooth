b4ade611c5a473ea32ca4389cb2e87a44b65557f
Jeff <jef.yabut.18@gmail.com.noreply.github.com>
2025-09-16 14:39:16 +0800

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Booth from "./Pages/Booth";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Booth" element={<Booth />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
